007586881081078dc52f53592b3ffb8685c64889
Rishabh Kumar <rishabhkaushal1796@gmail.com>
2024-09-13 10:33:37 +0530

    FILTERS: bilateralFilter - GaussianBlur - medianBlur

--- main.cpp
+++ main.cpp
@@ -7,11 +7,15 @@ int main(int argc, char **argv)
     cv::Mat image;
     image = cv::imread("/home/sage/Downloads/parasyte.jpg");
 
-    cv::Mat bilateralImg;
+    cv::Mat bilateralImg, gaussianImg, medianImg;
 
     cv::bilateralFilter(image, bilateralImg, 15, 95, 45);
+    cv::GaussianBlur(image, gaussianImg, cv::Size(15, 15), 0);
+    cv::medianBlur(image, medianImg, 15);
 
     cv::imshow("Bilateral", bilateralImg);
+    cv::imshow("Gaussian", gaussianImg);
+    cv::imshow("Median", medianImg);
     cv::imshow("Input", image);
     cv::waitKey(0);
 
